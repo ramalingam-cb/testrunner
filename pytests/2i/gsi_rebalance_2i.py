6449ea5c61f1655494e5a81048adccd6eed97e02
Ramalingam KalyanaSundaram <55530266+ramalingam-cb@users.noreply.github.com>
2019-10-14 18:30:43 +0530

    Update gsi_rebalance_2i.py

--- pytests/2i/gsi_rebalance_2i.py
+++ pytests/2i/gsi_rebalance_2i.py
@@ -2764,7 +2764,7 @@ class SecondaryIndexingRebalanceTests(BaseSecondaryIndexingTests, QueryHelperTes
                 rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],
                                                          [self.servers[self.nodes_init]], [],
                                                          services=services_in)
-            for x in range(230):
+            for x in range(1200):
               progress = self.rest._rebalance_progress()
               log.info(str(int(round(time.time() * 1000))))
               log.info(str(progress))
